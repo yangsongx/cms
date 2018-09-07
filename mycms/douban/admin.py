660b6c10a790e6e5c601429d8503578f18db8721
yangsx <yangsongxiang@shuzijiayuan.com>
2018-09-07 17:02:04 +0800

    a runnable admin website

--- mycms/douban/admin.py
+++ mycms/douban/admin.py
@@ -2,5 +2,15 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from douban.models import BookType, Book
 
 # Register your models here.
+class BookTypeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'type_name')
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+
+
+admin.site.register(BookType, BookTypeAdmin)
+admin.site.register(Book, BookAdmin)
--- mycms/douban/models.py
+++ mycms/douban/models.py
@@ -4,3 +4,14 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class BookType(models.Model):
+    type_name = models.CharField(max_length=100)
+
+
+class Book(models.Model):
+    book_type = models.ForeignKey(BookType)
+    title = models.CharField(max_length=512)
+    url = models.CharField(max_length=1024)
+    rate = models.FloatField(null=True)
+    pub = models.CharField(max_length=512)
