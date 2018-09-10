6f4460bd91c417c7f9f91d1634721f501de7129e
yangsx <yangsongxiang@shuzijiayuan.com>
2018-09-10 18:16:19 +0800

    1000 books

--- mycms/dbutil.py
+++ mycms/dbutil.py
@@ -29,28 +29,35 @@ class ImportData:
                 ret = ret + i
 
         return ret
+
     def load_in(self, jsobj):
 
         typeid = 1 # programming by default
         if jsobj['tag'] == 0:
             typeid = 1
+        elif jsobj['tag'] == 1:
+            typeid = 2
 
-        cmd = "INSERT INTO %s (title, url, rate, book_type_id, cover, pub) VALUES ('%s', '%s', %f, %d, '%s', 'TODO');" \
+        # check if it existed or not...
+        cmd = "SELECT url FROM %s WHERE url='%s'" %(self.__book_table, jsobj['url'])
+        existed = False
+        data = self.__dbcur.execute(cmd)
+        for it in data:
+            existed = True
+
+        if existed == False:
+         
+            cmd = "INSERT INTO %s (title, url, rate, book_type_id, cover, pub) VALUES ('%s', '%s', %f, %d, '%s', 'TODO');" \
                %(self.__book_table,
                  self.convert_quote(jsobj['title']) , jsobj['url'], jsobj['rating'] , typeid, jsobj['cover'])
-#        cmd = "INSERT INTO {} (title, url, rate, book_type_id, cover, pub) VALUES ({}, {}, {}, {}, {}, {});".format(\
-#               self.__book_table,
-#                 jsobj['title'] , jsobj['url'], jsobj['rating'] , typeid, jsobj['cover'], jsobj['author'])
-        print("the SQL cmd:%s" %(cmd))
-#        self.__dbcur.execute("INSERT INTO ? (title, url, rate, book_type_id, cover, pub) VALUES (?, ?, ?, ?, ?)",\
-#                (self.__book_table,
-#                 jsobj['title'] , jsobj['url'], jsobj['rating'] , typeid, jsobj['cover']))
-        self.__dbcur.execute(cmd)
+# print("the SQL cmd:%s" %(cmd))
+            self.__dbcur.execute(cmd)
+
         self.__dbconn.commit()
 
         return 0
 
-    def run(self):
+    def run_v0(self):
         with open(self.__jsfn, 'r') as f:
             full_data = f.read()
             jsdata = json.loads(full_data)
@@ -60,6 +67,18 @@ class ImportData:
             cmd = "DELETE FROM %s;" %(self.__book_table)
             self.__dbcur.execute(cmd)
             self.__dbconn.commit()
+
+            for it in jsdata:
+                self.load_in(it)
+
+        return 0
+
+    def run(self):
+        with open(self.__jsfn, 'r') as f:
+            full_data = f.read()
+            jsdata = json.loads(full_data)
+            print("json loaded[OK]")
+            print("there are totally %d items" %(len(jsdata)))
 
             for it in jsdata:
                 self.load_in(it)
--- mycms/douban/models.py
+++ mycms/douban/models.py
@@ -2,11 +2,17 @@
 from __future__ import unicode_literals
 
 from django.db import models
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 # Create your models here.
 
 class BookType(models.Model):
     type_name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.type_name
 
 
 class Book(models.Model):
@@ -16,3 +22,7 @@ class Book(models.Model):
     rate = models.FloatField(null=True)
     pub = models.CharField(max_length=512)
     cover = models.CharField(max_length=1024, default='')
+
+    def __str__(self):
+        return self.title
+
