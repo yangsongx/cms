2293b7c2cd893aef0f1b09606ab3e5f3668a36ae
Your Name <you@example.com>
2019-04-18 03:14:04 +0000

    go

--- mycms/financial/admin.py
+++ mycms/financial/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
-from financial.models import Fund
+from financial.models import Fund, Investment
 
 # Register your models here.
 
 class FundAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'fund_id', 'description')
 
+class InvestmentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'fund', 'base_money', 'when')
+
 admin.site.register(Fund, FundAdmin)
+admin.site.register(Investment, InvestmentAdmin)
--- mycms/financial/models.py
+++ mycms/financial/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Fund(models.Model):
@@ -9,15 +10,34 @@ class Fund(models.Model):
         verbose_name = '基金类型'
         verbose_name_plural = '基金类型'
 
-
 class Investment(models.Model):
+    ENVIRONMENT = (
+        (0, 'Gao'),
+        (1, 'Yang'),
+    )
+
     fund = models.ForeignKey(Fund, on_delete = models.CASCADE, blank=True, null=True)
     base_money =  models.FloatField(default=0.0)
-    when = models.DateTimeField(blank=True, null=True, default='1970-01-01 00:00:00')
+    who = models.IntegerField(u'投资人', choices=ENVIRONMENT, default=1)
+    when = models.DateTimeField(blank=True, null=True, default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     class Meta:
         verbose_name = '投资记录'
         verbose_name_plural = '投资记录'
+
+class Outcoming(models.Model):
+    ENVIRONMENT = (
+        (0, 'Gao'),
+        (1, 'Yang'),
+    )
+    fund = models.ForeignKey(Fund, on_delete = models.CASCADE, blank=True, null=True)
+    base_money =  models.FloatField(default=0.0)
+    who = models.IntegerField(u'撤资人', choices=ENVIRONMENT, default=1)
+    when = models.DateTimeField(blank=True, null=True, default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        verbose_name = '出资记录'
+        verbose_name_plural = '出资记录'
 
 class MoneyDetails(models.Model):
     fund = models.ForeignKey(Fund, on_delete = models.CASCADE, blank=True, null=True)
