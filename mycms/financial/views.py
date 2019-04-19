68ffc088d117ea745cbbd72101624f2da5ee2467
Your Name <you@example.com>
2019-04-19 05:47:16 +0000

    add some useful cms page

--- mycms/financial/views.py
+++ mycms/financial/views.py
@@ -1,42 +1,90 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
-from financial.models import Investment, MoneyDetails
+from financial.models import Investment, MoneyDetails, Outcoming
 
 import sys
 
 # Create your views here.
 
-def test_foo():
-    obj = MoneyDetails.objects.distinct()
+def calculate_how_much():
+    result = 0.0
+
+    obj = MoneyDetails.objects.values('fund').distinct()
+
     print(obj)
     print(len(obj))
     print("hello\n")
+
     for it in obj:
-        print(it.fund.name)
-        print(it.price * it.count)
+        print(it)
+        print("try check the fund id %d" %(it['fund']))
+        mobj = MoneyDetails.objects.filter(fund__id = it['fund']).order_by('-when')
+        print(mobj)
 
-    return 0
+        print(mobj[0].price * mobj[0].count)
+        result = result + (mobj[0].price * mobj[0].count)
 
+    print("=== end of dump details...")
+
+    return result
+
+# Entry point, for the HTML report
 def summary_entry(request):
     # First, process the data.
     try:
-        test_foo()
         obj = Investment.objects.all()
-        totally_money = 0.0
+        base_money = 0.0
+        out_money = 0.0
         real_money = 0.0
 
         totally_money_gao = 0.0 # id-0
         totally_money_yang = 0.0 # id-1
+        out_money_gao = 0.0
+        out_money_yang = 0.0
 
         render_dict_data = {
         }
 
         for it in obj:
-            totally_money = totally_money + it.base_money
+            base_money = base_money + it.base_money
+            if it.who == 0:
+                totally_money_gao = totally_money_gao + it.base_money
+            else:
+                totally_money_yang = totally_money_yang + it.base_money
+
+        # Next, need substract the outcoming
+        obj = Outcoming.objects.all()
+        for it in obj:
+            out_money = out_money + it.base_money
+            if it.who == 0:
+                out_money_gao = out_money_gao + it.base_money
+            else:
+                out_money_yang = out_money_yang + it.base_money
+
+        print("the base:%f, the out:%f" %(base_money, out_money))
+        base_money = base_money - out_money
+        totally_money_gao = totally_money_gao - out_money_gao
+        totally_money_yang = totally_money_yang - out_money_yang
+
+        print("the delta: %.2f" %(base_money))
+        print("So, there are totally %.2f money" %(base_money))
+
 
         # how much does it worth?
+        real_money = calculate_how_much()
 
-        print("There are totally %.2f money" %(totally_money))
+        real_money = float('%.2f' %(real_money))
+        base_money = float('%.2f' %(base_money))
+        totally_money_gao = float('%.2f' %(totally_money_gao))
+        totally_money_yang = float('%.2f' %(totally_money_yang))
+
+        render_dict_data['overview'] = {
+            'base_money': base_money,
+            'real_money': real_money,
+            'base_money_gao': totally_money_gao,
+            'base_money_yang': totally_money_yang
+        }
+
 
     except:
         msg = '%s || %s' %(sys.exc_info()[0], sys.exc_info()[1])
@@ -44,5 +92,5 @@ def summary_entry(request):
         return HttpResponse("exception met for summary: %s" %(msg))
    
 
-    return render(request, 'sum.html', {"mydic":{"mymoney": 20, "trows": 34}})
+    return render(request, 'sum.html', render_dict_data)
 
--- mycms/mycms/settings.py
+++ mycms/mycms/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '&@r)!zfpaxvfh5o$hk8kki8zqk2+%h7k3z3emz7u%vihpt#3cf'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['open.cn.hansong.io']
+ALLOWED_HOSTS = ['open.cn.hansong.io', '*']
 
 
 # Application definition
@@ -121,5 +121,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 STATIC_URL = '/static/'
+#STATIC_URL = 'static/'
+#STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+#    '/var/www/static/',
+]
